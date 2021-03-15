e0d9add97d0d63cde8816df6ec627869d92961bf
soca-git <soca.mailbox@gmail.com>
2021-03-15 00:46:05 +0000

    added dashboard template

---
+++ src/main/resources/templates/js/demo/chart-pie-demo.js
@@ -0,0 +1,42 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["AAPL", "QCOM", "NIO"],
+    datasets: [{
+      label: "Value",
+      data: [2500, 10000, 5000],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': $' + number_format(chart.datasets[0].data[tooltipItem['index']]);
+        }
+      }
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
