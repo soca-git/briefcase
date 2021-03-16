412a44312bf459e51b18454efaf79bb503d81855
Seán O'Callaghan <soca.mailbox@gmail.com>
2021-03-16 21:19:08 +0000

    added property files

--- src/main/java/com/example/demo/iexclient/IEXClient.java
+++ src/main/java/com/example/demo/iexclient/IEXClient.java
@@ -1,5 +1,7 @@
 package com.example.demo.iexclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import pl.zankowski.iextrading4j.api.stocks.Quote;
 import pl.zankowski.iextrading4j.client.*;
 import pl.zankowski.iextrading4j.client.rest.request.alternative.CryptoRequestBuilder;
@@ -13,15 +15,15 @@ public class IEXClient
     private final IEXCloudClient cloudClient;
     private final String version;
 
-    public IEXClient() {
+    public IEXClient()
+    {
         this.publicKey = "pk_d7996497b8844a999541274497663935";
-        //this.publicKey = "pk_f4a10817386c499cb82ae580e97dc795";
         this.secretKey = "sk_5b4f8a0d878245189977ce9a2d366c50";
-        this.token = new IEXCloudTokenBuilder().withPublishableToken(this.publicKey).build();
-        this.version = "IEX_CLOUD_V1";
-        this.cloudClient = IEXTradingClient.create(
+        token = new IEXCloudTokenBuilder().withPublishableToken(publicKey).build();
+        version = "IEX_CLOUD_V1";
+        cloudClient = IEXTradingClient.create(
                 IEXTradingApiVersion.IEX_CLOUD_V1,
-                this.token
+                token
         );
     }
 
